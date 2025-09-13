19564cbf945ae1778304c08a7d578ad820ef775c
burak <genc.burak@gmail.com>
2025-09-13 22:55:12 +0300

    mimari düzelmte öncesi

---
+++ Core/MyApi.Application/Services/Abstract/Identity/IClaimService.cs
@@ -0,0 +1,17 @@
+﻿using MyApi.Application.DTOs.Account;
+using MyApi.Application.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApi.Application.Services.Abstract.Identity
+{
+    public interface IClaimService
+    {
+        Task<IResult> AddClaimToUserAsync(Guid userId, ClaimDto claim);
+        Task<IResult> RemoveClaimFromUserAsync(Guid userId, ClaimDto claim);
+        Task<IDataResult<IEnumerable<ClaimDto>>> GetUserClaimsAsync(Guid userId);
+    }
+}
---
+++ Core/MyApi.Application/Services/Abstract/Identity/IRoleService.cs
@@ -0,0 +1,19 @@
+﻿using MyApi.Application.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApi.Application.Services.Abstract.Identity
+{
+    public interface IRoleService
+    {
+        Task<IDataResult<IEnumerable<string>>> GetRolesAsync();
+        Task<IResult> AddRoleAsync(string roleName);
+        Task<IResult> DeleteRoleAsync(string roleName);
+        Task<IResult> AssignRoleToUserAsync(Guid userId, string roleName);
+        Task<IResult> RemoveRoleFromUserAsync(Guid userId, string roleName);
+        Task<IDataResult<IEnumerable<string>>> GetUserRolesAsync(Guid userId);
+    }
+}
